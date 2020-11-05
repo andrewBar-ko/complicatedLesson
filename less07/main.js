3c52356166cc9193c17b6ef37c8b95d0dc619a17
Andrew Barkovets <andrewbarkovets@gmail.com>
2020-11-05 06:46:16 +0300

    feat(main.js): new feat

--- less07/main.js
+++ less07/main.js
@@ -1,1 +1,29 @@
 'use strict';
+
+// Создать массив week
+// и записать в него дни недели в виде строк!
+
+const week = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'];
+
+const day = new Date();
+let weekDay = day.getDay();
+
+if ( weekDay === 0) { weekDay = 6; } else { weekDay--; }
+
+week.forEach((day, i) => {
+
+    let str = day;
+
+    if (i === weekDay) {
+        str = `<b>${day}</b>`;
+    } else {
+        str = `${day}`;
+    }
+
+    if (i === 5 || i === 6) { 
+        str = `<i>${str}</i>`; 
+    }
+
+    document.body.insertAdjacentHTML('beforeend', `<div>${str}</div>`);
+
+});
