4c791dd0ca192e705518cd1e59d595bec2b1d2f9
Andrew Barkovets <andrewbarkovets@gmail.com>
2020-11-12 19:35:53 +0300

    fix(main.js): new fix

--- bonusWeek/script/main.js
+++ bonusWeek/script/main.js
@@ -1,14 +1,14 @@
 'use strict';
 
 const registerUser = document.querySelector('#register-user'),
-login = document.querySelector('#login'),
-registeredName = document.querySelector('#registered-name'),
-list = document.querySelector('#list'),
-listItem = document.querySelector('#list-item'),
-deleteUser = document.querySelector('#delete-user');
+    login = document.querySelector('#login'),
+    registeredName = document.querySelector('#registered-name'),
+    list = document.querySelector('#list'),
+    deleteUser = document.querySelector('#delete-user');
 
 // Дата регистрации
 const dateRegist = {
+
     year: 'numeric',
     month: 'long',
     day: 'numeric',
@@ -17,36 +17,38 @@ const dateRegist = {
     hour: 'numeric',
     minute: 'numeric',
     second: 'numeric'
-  };
 
-  const registName = /^[a-zA-Zа-яА-Я'][a-zA-Zа-яА-Я-' ]+[a-zA-Zа-яА-Я']?$/;
+};
+
+const registName = /^[a-zA-Zа-яА-Я'][a-zA-Zа-яА-Я-' ] + [a-zA-Zа-яА-Я']?$/;
 
 // Массив с получеными данными о планах
-let listData = localStorage.getItem('item') ?
+let listData = localStorage.getItem('listData') ?
 // извличение из JSON и декодировка в массив
- JSON.parse(localStorage.getItem('item')) : [];
+JSON.parse(localStorage.getItem('listData')) : [];
 
- const addToStorage = () => {
+const addToStorage = () => {
     localStorage.setItem('listData', JSON.stringify(listData));
-  };
-
+};
+addToStorage();
 
  // Добавление в список
 const register = () => {
 
-    listData.textContent = '';
+    list.textContent = '';
    
-
     listData.forEach((item) => {
         let li = document.createElement('li');
         li.classList.add('list-item');
         
-        li.innerHTML += '<span>' + 'Имя:' + 
-        item.name + ', фамилия: ' + 
-        item.lastName + ', зарегистрирован: ' + 
+        li.innerHTML += '<span>' + '<b>Имя:</b>' + 
+        item.userName + '  <b>фамилия:</b> ' + 
+        item.lastName + '  <b>зарегистрирован:</b> ' +  
         item.regDate + '</span>' + '  ' +
         '<a href="#" class="btn btn-delete" id="delete-user">' +
          'X' + '</a>';
+
+        list.append(li);
         
         // Удаление дел
         const deleteUser = li.querySelector('#delete-user');
@@ -58,11 +60,13 @@ const register = () => {
                     if (listData[i] === item) {
                         listData.splice(i, 1);
                     }
+                    
+                    localStorage.removeItem(listData[i]);
+                    register();
+                    addToStorage(); 
                 }
+                
             });
-            
-            localStorage.setItem('item', JSON.stringify(listData));
-             
             register();
         });
 
@@ -71,22 +75,26 @@ const register = () => {
     const start = () => {
 
         let promptName = prompt('Введите через пробел Имя и Фамилию пользователя');
+
+        if (promptName === null) {
+            return;
+        }
       
         if (!registName.test(promptName) || 
             promptName.split(' ').length - 1 > 1 || 
             promptName.split(' ').length - 1 < 1) {
 
-          alert('Ошибка ввода данных!');
-          return;
+            alert('Ошибка ввода данных!');
+            return;
         } 
     
-        let loginUser = prompt('Введите логин');
+        let promptLogin = prompt('Введите логин');
     
-        if (loginUser === null) {
-          return;
+        if (promptLogin === null) {
+            return;
         }
     
-        let passUser = prompt('Введите пароль');
+        let promptPass = prompt('Введите пароль');
     
         const arr = promptName.split(' ');
     
@@ -94,83 +102,53 @@ const register = () => {
         const toDay = date.toLocaleString('ru', dateRegist);
     
         const newText = {
-          userName: arr[0],
-          lastName: arr[1], 
-          login: loginUser,
-          password: passUser,
-          regDate: toDay
+            userName: arr[0],
+            lastName: arr[1], 
+            login: promptLogin,
+            password: promptPass,
+            regDate: toDay
         };
           
         listData.push(newText);
         register();
         addToStorage();
         registerUser.removeEventListener('click', start);
-      };
+    };
     
-      registerUser.addEventListener('click', start); 
+    registerUser.addEventListener('click', start); 
 
 };
 
-
 const authorization = () => {
+
     let promptLogin = prompt('Введите логин');
   
     if (promptLogin === null) {
       return;
     }
   
-    let pasAuto = prompt('Введите пароль');
+    let promptPass = prompt('Введите пароль');
   
-    if (pasAuto === null) {
+    if (promptPass === null) {
       return;
     }
   
     let promptLoginValue;
     listData.forEach((item) => { 
-      if (item.login === promptLogin && 
-        item.password === pasAuto) {
-        promptLoginValue = promptLogin;
-      } 
-      console.log(promptLoginValue);
+        if (item.login === promptLogin && 
+            item.password === promptPass) {
+            promptLoginValue = promptLogin;
+        } 
+
     });
   
     if (promptLoginValue === undefined) {
-      alert('Пользователь не найден');
+        alert('Пользователь не найден');
     } else {
         registeredName.textContent = promptLogin;
     }
-  };
+};
 
-  login.addEventListener('click', authorization);
-
-// // К форме todoControl навешиваем событие submit
-// registerUser.addEventListener('click', function(e) {
-//     e.preventDefault();  // Отмена перезагрузки страницы
-
-
-//     let promptName = prompt('Введите Имя и Фамилию через пробел!');
-//     let userLogin = prompt('Введите логин!');
-//     let userPass = prompt('Введите пароль!');
-    
-//     // Новое дело
-//     const newList = {
-//         value: userName.value,
-//         value: userLogin.value,
-//         value: userPass.value,
-         
-//     };
-
-//     // Проверка на пустоту и добавление в массив
-//     if(promptName.trim() !== '') {
-//         listData.push(newList);
-//     }
-    
-//     // Кодировка массива в JSON и сохранение в обьекте localStorage
-//     localStorage.setItem('item', JSON.stringify(listData));
-//     promptName.value = '';
-     
-//     register();
- 
-// });
+login.addEventListener('click', authorization);
 
 register();
